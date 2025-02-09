edf5a2266f524fba75020eeb236ae96e6f02ae87
Sushmita-purtgeri <purtgerisushmita@gmail.com>
2025-02-09 06:52:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //traet all JS code as newer version.
+
+//alert(3+3) // we are using node js not browser.
+
+//code readability
+console.log(3+3);
+
+let name="Sushmita"
+let age=18
+let isLoggedIn = false
+
+//number 
+//bigint
+//string ==> ""
+//boolean ==>true/false
+//null => standalone value
+//undefined
+//symbol => uniqe
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
